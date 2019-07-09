83caba4e01c183b7213fe2001d75c73dde610ef7
huangchunchao <chunchao.huang@transwarp.io>
2019-07-09 14:19:27 +0800

    将redis连接方式设置为lettuce连接池

--- src/test/java/com/example/demo/SecurityApplicationTests.java
+++ src/test/java/com/example/demo/SecurityApplicationTests.java
@@ -24,23 +24,15 @@ public class SecurityApplicationTests {
 	private RedisTemplate<String,User> redisTemplate;
 	@Test
 	public void testStringRedisTemplate() throws Exception {
-		stringRedisTemplate.opsForValue().set("aaa", "111");
-		Assert.assertEquals("111",stringRedisTemplate.opsForValue().get("aaa"));
+		stringRedisTemplate.opsForValue().set("aaaa", "111");
+		Assert.assertEquals("111",stringRedisTemplate.opsForValue().get("aaaa"));
 	}
 	
 	@Test
 	public void testRedisTemplate() throws Exception {
-        User user = new User("超人", 20);
+        User user = new User("csss", 20);
         redisTemplate.opsForValue().set(user.getUsername(), user);
 
-        user = new User("蝙蝠侠", 30);
-        redisTemplate.opsForValue().set(user.getUsername(), user);
-
-        user = new User("cc", 40);
-        redisTemplate.opsForValue().set(user.getUsername(), user);
-
-        Assert.assertEquals(20, redisTemplate.opsForValue().get("超人").getAge().longValue());
-        Assert.assertEquals(30, redisTemplate.opsForValue().get("蝙蝠侠").getAge().longValue());
-        Assert.assertEquals(40, redisTemplate.opsForValue().get("cc").getAge().longValue());
+        Assert.assertEquals(20, redisTemplate.opsForValue().get("csss").getAge().longValue());
 	}
 }
